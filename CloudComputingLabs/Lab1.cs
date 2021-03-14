f58486da44eeb71e22e847a01389c6c31a788e09
Ilya Axenov <31668898+FokusHuk@users.noreply.github.com>
2021-03-14 21:00:55 +0300

    lab 1

---
+++ CloudComputingLabs/Lab1.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudComputingLabs
+{
+    public class Lab1
+    {
+        public int PureSum(int[] array, int sum = 0, int currentIndex = 0) => 
+            currentIndex == array.Length ? sum : PureSum(array, sum + array[currentIndex], ++currentIndex);
+    }
+}
---
+++ CloudComputingLabs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CloudComputingLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteLab1();
+        }
+
+        static void ExecuteLab1()
+        {
+            var lab1 = new Lab1();
+            var array = new[] {1, 2, 3, 4, 5, 6, 7, 8};
+
+            var result = lab1.PureSum(array);
+
+            Console.WriteLine(result); // 36
+        }
+    }
+}
